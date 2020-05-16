e76296ee0646961f1cea571197147033a71fdf6e
iamuv2000 <yuvraj.singh2018@vitstudent.ac.in>
2020-05-17 03:10:41 +0530

    Converted to function, handling case insesitivity

--- movie_recommender.py
+++ movie_recommender.py
@@ -5,69 +5,69 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+def get_movie_recommendation(movie_user_like, no_of_movies):
+    #Read csv
+    dataset = pd.read_csv('movie_dataset.csv')
 
-#Read csv
-dataset = pd.read_csv('movie_dataset.csv')
-print(dataset.head())
+    # Helper functions
 
+    #Get Title from index
+    def get_title_from_index(index):
+        return dataset[dataset.index == index]['title'].values[0]
 
-# Helper functions
-
-#Get Title from index
-def get_title_from_index(index):
-    return dataset[dataset.index == index]['title'].values[0]
-
-#Get index from title
-def get_index_from_title(title):
-    return dataset[dataset.title == title]['index'].values[0]
-
-
-#Select Features
-features = ['keywords' , 'cast' , 'genres' , 'director']
+    #Get index from title
+    def get_index_from_title(title):
+        title = title.lower()
+        dataset['title'] = dataset['title'].str.lower() 
+        return dataset[dataset.title == title]['index'].values[0]
 
 
-# Clean data
-for feature in features:
-    dataset[feature] = dataset[feature].fillna('')
+    #Select Features
+    features = ['keywords' , 'cast' , 'genres' , 'director']
 
 
-#Create column in dataframe which combines all the selected features
-def combine_features(rows):
-    return rows['keywords'] + " " +  rows['cast'] + " " +  rows['genres'] + " " +  rows['director']
-
-dataset["combined_features"] = dataset.apply(combine_features , axis = 1)
-
-# Create count feature from this new combined column
-cv = CountVectorizer()
-
-count_matrix = cv.fit_transform(dataset["combined_features"])
-
-# Calculate cosine similarity based on count_matrix
-cosine_sim = cosine_similarity(count_matrix)
-
-movie_user_like = 'The Proposal'
-
-# Get index of movie from title
-movie_index = get_index_from_title(movie_user_like)
+    # Clean data
+    for feature in features:
+        dataset[feature] = dataset[feature].fillna('')
 
 
-# Find similar movie in descending order of similarity score
-similar_movie = list(enumerate(cosine_sim[movie_index]))
-sorted_similar_movies = sorted(similar_movie, key = lambda x:x[1], reverse = True)
+    #Create column in dataframe which combines all the selected features
+    def combine_features(rows):
+        return rows['keywords'] + " " +  rows['cast'] + " " +  rows['genres'] + " " +  rows['director']
 
-i = 0
-for movie in sorted_similar_movies:
-    print(get_title_from_index(movie[0]))
-    i = i+1
-    if i > 20:
-        break
+    dataset["combined_features"] = dataset.apply(combine_features , axis = 1)
 
+    # Create count feature from this new combined column
+    cv = CountVectorizer()
+
+    count_matrix = cv.fit_transform(dataset["combined_features"])
+
+    # Calculate cosine similarity based on count_matrix
+    cosine_sim = cosine_similarity(count_matrix)
+
+    # Get index of movie from title
+    try:
+        movie_index = get_index_from_title(movie_user_like)
+    except:
+        print("Movie not found in database")
+        return 0
+
+
+    # Find similar movie in descending order of similarity score
+    similar_movie = list(enumerate(cosine_sim[movie_index]))
+    sorted_similar_movies = sorted(similar_movie, key = lambda x:x[1], reverse = True)
+
+    i = 0
+    for movie in sorted_similar_movies:
+        print(get_title_from_index(movie[0]).title())
+        i = i + 1
+        if i > no_of_movies:
+            break
+
+
+get_movie_recommendation('The proposal', 4)
 
 #TODO: Test accuracy with release date
-#TODO: Clean search term (ignore case)
 #TODO: Use cosine similarity to auto detect close enough search term
-#TODO: Add title not found case
-#TODO: Allow variable number of recommendations
-#TODO: Make a parent function
 #TODO: Make a flask endpoint
 #TODO: Make a frontend
